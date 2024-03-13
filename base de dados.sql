b4306c205781542a90d1f007abc78607ab19171d
Miquéias Rodrigues <84649194+miqueiasrodrigues@users.noreply.github.com>
2024-03-12 23:26:49 -0300

    Update base de dados.sql

---
+++ base de dados.sql
@@ -0,0 +1,14 @@
+-- Database: gerenciamento_clientes
+
+-- DROP DATABASE IF EXISTS gerenciamento_clientes;
+
+CREATE DATABASE gerenciamento_clientes
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
